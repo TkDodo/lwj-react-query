d46f184aec742efd6ef8e6e5843b2f3c8e80c850
Dominik Dorfmeister <office@dorfmeister.cc>
2022-12-20 21:13:28 +0100

    search

---
+++ src/Search.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+export default function Search({
+  onSearch,
+  defaultValue,
+}: {
+  onSearch: (query: string) => void;
+  defaultValue: string;
+}) {
+  const handleSubmit: React.ComponentProps<"form">["onSubmit"] = (event) => {
+    event.preventDefault();
+    onSearch(new FormData(event.currentTarget).get("search") as string);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        defaultValue={defaultValue}
+        type="search"
+        name="search"
+        placeholder="Search products"
+        autoFocus
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+}
